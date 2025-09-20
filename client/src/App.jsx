43c6f866ed5d877792e81fe6be7ec0a4bbfcf3c3
SrMesa1 <118227323+SrMesa1@users.noreply.github.com>
2025-09-19 22:04:44 -0500

    configuracion inicial de docker

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Firstpages from './Componentes/Firstpages';
+import Login from './Componentes/Login';
+import Register from './Componentes/Register';
+import './App.css';
+
+function App() {
+
+  return (
+    
+    <div>
+       <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Firstpages />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+    </BrowserRouter>
+    </div>
+      
+   
+  )
+}
+
+export default App
---
+++ client/src/Componentes/Firstpages.jsx
@@ -0,0 +1,33 @@
+
+import { Link } from 'react-router-dom';
+
+
+const Firstpages = () => {
+  return (
+    <div className="firstpages-main">
+        <div className='description-conatiner'>
+            <img src="" alt="Foto Pacual" className="firstpages-img"/>
+            <h1 className="firstpages-title">Gestion de practicas profesionales</h1>
+            <p>Este sitio esta creado para que los estudiiantes puedan 
+               ver ofertas de empresas que estan buscando practicantes,
+               y a su vez las empresas puedan publicar sus ofertas de practicas
+               profesionales para que los estudiantes puedan postularse.
+            </p>
+        </div>
+        
+        <div className='options-container'>
+            <div className="firstpages-buttons">
+                 <h2>Ir a registrarse</h2>
+                <Link to="/register" className="firstpages-btn">Entra aqui</Link>
+            </div>
+           
+            <div className="firstpages-buttons">
+                 <h2>Iniciar sesion</h2>
+                 <Link to="/login" className="firstpages-btn">Entra aqui</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Firstpages
